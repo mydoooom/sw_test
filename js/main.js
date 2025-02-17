28d3eb273b9493a1a88e7dda0bfe99c1d8045318
Tran Viet Anh <tran@vietanh.cz>
2025-02-17 15:05:16 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const paragraph = document.createElement("p")
+paragraph.textContent =  "Text from javascript"
+document.body.appendChild(paragraph)
+
+if ("serviceWorker" in navigator) {
+  // Register SW on load
+  window.addEventListener("load", () => {
+    // Registering our SW
+    navigator.serviceWorker
+      .register("sw_cached_site.js")
+      .then((registration) => console.log("Service worker registered", registration))
+      .catch((error) => console.log(`Service worker error: ${error}`));
+  })
+}
