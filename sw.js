c2b25e581d24f76b400a8b33d489c9d2928d41d1
Tran Viet Anh <tran@vietanh.cz>
2025-02-24 19:58:22 +0100

    change behavior

--- sw.js
+++ sw.js
@@ -19,12 +19,14 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => {
-    return Promise.all(keys
-      .filter(key => key !== cacheName)
-      .map(key => caches.delete(key))
-    )
-  }))
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      )
+    })
+  )
 })
 
 self.addEventListener('fetch', event => {
